d41149cc1506faa6a9e9ece755803e5ff30aba2d
Aude Forcione-Lambert <kijrel@gmail.com>
2016-01-14 14:04:11 -0500

    ajouté les fichiers

---
+++ js/app-angular.js
@@ -0,0 +1,39 @@
+(function() {
+  var app = angular.module('physum',[]);
+  
+  app.controller('MembresCa', ['$http', function($http){
+    var equipe = this;
+    equipe.membres = [];
+    $http.get('/v_beta/json/membres-exec.json').success(function(data){
+      equipe.membres=data;
+    });
+  }]);
+  
+  app.controller('TabController', function(){
+    this.tab = -1;
+    
+    this.setTab = function(newValue){
+      if (this.tab == newValue){
+        this.tab = -1;
+      }
+      else{
+        this.tab = newValue;
+      }
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+  
+  app.controller('AlbumCtrlTest', ['$http', function($http){
+    var scope = this;
+    scope.albums = [];
+    $http.get('/v_beta/json/albums.json').success(function(data){
+      scope.albums=data;
+    });
+    this.tab=1;
+    
+  }]);
+  
+})();
