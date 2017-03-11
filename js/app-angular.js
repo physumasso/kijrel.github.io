257cd7a640b4f69eaf14ff1dfae878bd69871777
Aude Forcione-Lambert <kijrel@gmail.com>
2017-03-11 11:13:42 -0500

    prise 2

--- js/app-angular.js
+++ js/app-angular.js
@@ -5,7 +5,7 @@
     var equipe = this;
     equipe.membres = [];
     equipe.tab=[];
-    $http.get('/kijrel.github.io/json/membres-exec.json').success(function(data){
+    $http.get('./json/membres-exec.html').success(function(data){
       equipe.membres = data;
       for(i=0;i<equipe.membres.length;i++){
         equipe.tab[i] = false;
@@ -30,7 +30,7 @@
   app.controller('Comites', ['$http', function($http){
     var scope = this;
     scope.com = [];
-    $http.get('/kijrel.github.io/json/comites.json').success(function(data){
+    $http.get('./json/comites.html').success(function(data){
       scope.com=data;
     });
   }]);
@@ -38,7 +38,7 @@
   app.controller('Evenements', ['$http', function($http){
     var scope = this;
     scope.eve = [];
-    $http.get('/kijrel.github.io/json/evenements.json').success(function(data){
+    $http.get('./json/evenements.html').success(function(data){
       scope.eve=data;
     });
   }]);
